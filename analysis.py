10febfa8729e464787e510494792f4358166cd0c
Daniel <skalskidaniel@icloud.com>
2025-05-25 22:37:08 +0200

    add tables

--- analysis.py
+++ analysis.py
@@ -1,7 +1,13 @@
 import kagglehub
+import pandas as pd
+import os
 
-# data = kagglehub.dataset_download("limtis/wikiliq-dataset")
+dataset_path = kagglehub.dataset_download("limtis/wikiliq-dataset")
 
-data = kagglehub.load_dataset(kagglehub.KaggleDatasetAdapter.PANDAS, "limtis/wikiliq-dataset", "beer_data.csv")
+data_beer = pd.read_csv(os.path.join(dataset_path, "beer_data.csv"))
+data_spirits = pd.read_csv(os.path.join(dataset_path, "spirits_data.csv"))
+data_wine = pd.read_csv(os.path.join(dataset_path, "wine_data.csv"))
 
-print(data)
+print(data_beer.head())
+print(data_spirits.head())
+print(data_wine.head())
