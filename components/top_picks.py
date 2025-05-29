2dd77d3ce780a47621dc81f5831e4175bf509b1a
Wiktor <wiktal123@gmail.com>
2025-05-29 15:55:57 +0200

    merge skill issue fixed

--- components/top_picks.py
+++ components/top_picks.py
@@ -9,22 +9,4 @@ def create_top_picks(data: pd.DataFrame):
             html.H3("Our top picks"),
             html.Hr(),
             dbc.Table.from_dataframe(q, striped=True, bordered=True, hover=True, id='top-picks-table') # there is not really a problem, it works
-            dash_table.DataTable(
-                data=data.head(15).to_dict('records'),
-                columns=[{"name": i, "id": i} for i in {"Name", "Type", "Price", "Country", "Rating"}],
-                page_size=15,
-                style_table={
-                    'overflowX': 'auto',
-                    'width': '100%',
-                    'minWidth': '100%',
-                },
-                style_cell={
-                    'textAlign': 'left',
-                    'minWidth': '120px', 
-                    'width': '120px',
-                    'maxWidth': '200px',
-                    'whiteSpace': 'normal'
-                }
-            )
-        ], className="bg-light border rounded m-3 p-3"
-    )
+        ], className="bg-light border rounded m-3 p-3")
