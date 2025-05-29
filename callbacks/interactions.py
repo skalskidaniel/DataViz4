3d2cdfa0ec1313537c670fa4366f76ba566b136a
Daniel <skalskidaniel@icloud.com>
2025-05-29 15:33:47 +0200

    implement top picks

--- callbacks/interactions.py
+++ callbacks/interactions.py
@@ -1,5 +1,6 @@
-from dash import Input, Output, Dash
-from data.loader import extract_unique_values, load_data
+from dash import Input, Output, Dash, html
+from data.loader import extract_unique_values, load_data, query_data
+import pandas as pd
 
 def register_callbacks(app: Dash):
     
@@ -18,6 +19,34 @@ def register_callbacks(app: Dash):
         
         return categories, taste, food, len(food) <= 0
     
-    #TODO add main table callbacks
+    
+    @app.callback(
+        Output('top-picks-table', 'children'),
+        Input('type', 'value'),
+        Input('category', 'value'),
+        Input('taste-filter', 'value'),
+        Input('food-filter', 'value'),
+        Input('price-range', 'value')
+    )
+    def generate_top_picks(type: str, category: str, tastes: list[str], food: list[str], price_range: list[int]):
+        
+        data = load_data(type)
+    
+        q = query_data(data, category, tastes, food, price_range)
+        
+        if q.empty:
+            return [
+                html.Thead(html.Tr([html.Th(col) for col in ["Name", "Type", "Price", "Rating"]])),
+                html.Tbody(html.Tr([html.Td('No results found.', colSpan=4, className="text-center")]))
+            ]
+        
+        header = html.Thead(html.Tr([html.Th(col) for col in q.columns]))
+        
+        rows = []
+        for _, row in q.iterrows():
+            rows.append(html.Tr([html.Td(row[col]) for col in q.columns]))
+        body = html.Tbody(rows)
+        
+        return [header, body]
     
     #TODO add overview callbacks
--- components/top_picks.py
+++ components/top_picks.py
@@ -1,12 +1,12 @@
 from dash import html, dcc, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
+from data.loader import query_data
 
-#TODO
 def create_top_picks(data: pd.DataFrame):
+    q = query_data(data, "", [], [], [0, 200])
     return dbc.Col([
             html.H3("Our top picks"),
             html.Hr(),
-            html.P("There will be a table displayed", className='text-center fw-bold fst-italic'),
-            html.P("It will probably contain top 5 results with only few columns", className='text-center fst-italic')
+            dbc.Table.from_dataframe(q, striped=True, bordered=True, hover=True, id='top-picks-table') # there is not really a problem, it works
         ], className="bg-light border rounded m-3 p-3")
