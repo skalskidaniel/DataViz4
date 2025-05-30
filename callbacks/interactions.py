f5ef0c63de770db71ff4e53ed5b57f82a5a05060
Daniel <skalskidaniel@icloud.com>
2025-05-30 19:53:03 +0200

    fix callbacks

--- callbacks/interactions.py
+++ callbacks/interactions.py
@@ -6,34 +6,37 @@ from components.overview import create_overview
 def register_callbacks(app: Dash):
     
     @app.callback(
+        Output('category', 'value'),
+        Output('country', 'value'),
+        Output('taste-filter', 'value'),
+        Output('food-filter', 'value'),
+        Input('type', 'value')
+    )
+    def changed_drink_type(type):
+        return None, None, None, None
+    
+    @app.callback(
     Output('category', 'options'),
+    Output('country', 'options'),
     Output('taste-filter', 'options'),
     Output('food-filter', 'options'),
     Output('food-filter', 'disabled'),
-    Input('type', 'value')
+    Output('top-picks-table', 'children'),
+    Input('type', 'value'),
+    Input('category', 'value'),
+    Input('country', 'value'),
+    Input('taste-filter', 'value'),
+    Input('food-filter', 'value'),
+    Input('price-range', 'value')
     )
-    def set_drink_type(type):
+    def sidebar_callbacks(type, category, country, tastes, foods, price_range):
         data = load_data(type)
-        categories = extract_unique_values(data, 'Categories')
-        taste = extract_unique_values(data, 'Tasting Notes')
-        food = extract_unique_values(data, 'Food Pairing') if type == 'Wine' else []
+        filtered_data = get_filtered_items(data, category, country, tastes, foods, price_range)
+        categories = extract_unique_values(filtered_data, 'Categories')
+        countries = extract_unique_values(filtered_data, 'Country')
+        taste = extract_unique_values(filtered_data, 'Tasting Notes')
+        food = extract_unique_values(filtered_data, 'Food Pairing') if type == 'Wine' else []
         
-        return categories, taste, food, len(food) <= 0
-    
-    
-    @app.callback(
-        Output('top-picks-table', 'children'),
-        Input('type', 'value'),
-        Input('category', 'value'),
-        Input('country', 'value'),
-        Input('taste-filter', 'value'),
-        Input('food-filter', 'value'),
-        Input('price-range', 'value')
-    )
-    def generate_top_picks(type: str, category: str, country: str, tastes: list[str], food: list[str], price_range: list[int]):
-        
-        data = load_data(type)
-        filtered_data = get_filtered_items(data, category, country, tastes, food, price_range)
         top_items = get_top_scored_items(filtered_data)
         cols = ["Name", "Country", "Price", "Rating", "Score"]
         top_items = top_items[cols].head(13)
@@ -51,21 +54,22 @@ def register_callbacks(app: Dash):
             rows.append(html.Tr([html.Td(row[col]) for col in top_items.columns]))
         body = html.Tbody(rows)
         
-        return [header, body]
+        return categories, countries, taste, food, len(food) <= 0, [header, body]
     
-    @app.callback(
-        Output('overview', 'children'),
-        Input('type', 'value'),
-        Input('category', 'value'),
-        Input('country', 'value'),
-        Input('taste-filter', 'value'),
-        Input('food-filter', 'value'),
-        Input('price-range', 'value')
-    )
-    def generate_overview(type: str, category: str, country: str, tastes: list[str], food: list[str], price_range: list[int]):
         
-        data = load_data(type)
-        filtered_data = get_filtered_items(data, category, country, tastes, food, price_range)
-        top_item = get_top_scored_items(filtered_data).iloc[0]
+    # @app.callback(
+    #     Output('overview', 'children'),
+    #     Input('type', 'value'),
+    #     Input('category', 'value'),
+    #     Input('country', 'value'),
+    #     Input('taste-filter', 'value'),
+    #     Input('food-filter', 'value'),
+    #     Input('price-range', 'value')
+    # )
+    # def generate_overview(type: str, category: str, country: str, tastes: list[str], food: list[str], price_range: list[int]):
         
-        return create_overview(top_item, data)
+    #     data = load_data(type)
+    #     filtered_data = get_filtered_items(data, category, country, tastes, food, price_range)
+    #     top_item = get_top_scored_items(filtered_data).iloc[0]
+        
+    #     return create_overview(top_item, data)
