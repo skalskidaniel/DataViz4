7083b17d30f2777c2f96901340eb8e44c984ea40
Daniel <skalskidaniel@icloud.com>
2025-05-31 18:52:59 +0200

    add map

--- components/map.py
+++ components/map.py
@@ -1,38 +1,98 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
 def create_map(data: pd.DataFrame):
     
-    country_counts = data['Country'].value_counts().reset_index()
-    country_counts.columns = ['Country', 'Producer_Count']
+    data = data.copy()
+    data['Price_num'] = (
+        data['Price']
+        .astype(str)
+        .str.replace(r'[\$,]', '', regex=True)
+        .astype(float, errors='ignore')
+    )
+    
+    countries = (
+        data
+        .groupby('Country')
+        .agg(
+            Count=('Name','count'),
+            Avg_Rating=('Rating','mean'),
+            Avg_Price=('Price_num','mean')
+        )
+        .reset_index()
+    )
+    countries['Count_log'] = np.log1p(countries['Count'])
+    
     
     fig = px.choropleth(
-        country_counts,
+        countries,
         locations='Country',
-        color='Producer_Count',
+        color='Count_log',
         locationmode='country names',
         color_continuous_scale='Blues',
-        title='Number of Producers by Country',
-        labels={'Producer_Count': 'Number of Producers'},
-        hover_data={'Country': True, 'Producer_Count': True}
+        labels={'Count_log': 'Number of products (Log scale)'},
+        hover_data={
+            'Count': True,
+            'Avg_Rating':':.2f',
+            'Avg_Price':':.2f'
+        },
+        custom_data=['Count', 'Avg_Rating', 'Avg_Price']
+    )
+    
+    fig.update_geos(
+        showcountries=True,
+        showcoastlines=True,
+        showland=True,
+        landcolor="#f8f9fa",
+        resolution=50,
+        fitbounds='locations',
+        scope='world',
+        bgcolor="#f8f9fa"
+    )
+    
+    fig.update_traces(
+        hovertemplate=(
+            '<b>%{location}</b><br>'
+            'Number of products: %{customdata[0]}<br>'
+            'Mean rating: %{customdata[1]:.2f}<br>'
+            'Mean price: %{customdata[2]:.2f}<br>'
+            '<extra></extra>'
+        )
     )
     
     fig.update_layout(
-        geo=dict(
-            showframe=False,
-            showcoastlines=True,
-            projection_type='equirectangular'
-        ),
-        title_x=0.5,
-        height=500,
-        margin=dict(l=0, r=0, t=50, b=0)
+        geo=dict(projection_type='robinson'),
+        margin=dict(l=0, r=0, t=0, b=0),
+        plot_bgcolor="#f8f9fa",
+        paper_bgcolor="#f8f9fa",
+        autosize=True,
+        coloraxis_colorbar=dict(
+            title="Product Count (logarithmic scale)",
+            orientation='h',           
+            x=0.5,                     
+            xanchor='center',
+            y=-0.02,                      
+            yanchor='top',
+            thickness=15,
+            len=0.5,
+            tickvals=[1, 9],
+            ticktext=['1', '9']
+        )
     )
     
     return dbc.Row([
         html.H3("Map of producers"),
-        html.Hr(),
-        dcc.Graph(figure=fig, config={'displayModeBar': False})
+        html.Hr(className="mb-3"),
+        dcc.Graph(figure=fig, 
+                  style={"width": "100vw", "height": "90vh"},
+                  config={
+                        'displaylogo': False,
+                        'modeBarButtonsToRemove': ['zoom', 'pan', 'select', 'lasso2d', 'toImage'],
+                        'scrollZoom': False
+                    }
+                 )
     ], className="bg-light border rounded m-3 p-3")
